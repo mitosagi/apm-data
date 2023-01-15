ea437b20ae5b8236ba969d0d097d84a906f50846
mitosagi <54105954+mitosagi@users.noreply.github.com>
2023-01-15 11:19:35 +0900

    fix: specify shift_jis when linux

--- util/lib/unzip.js
+++ util/lib/unzip.js
@@ -1,6 +1,8 @@
 import { path7za } from '7zip-bin';
 import Seven from 'node-7z';
+import { execSync } from 'child_process';
 const { extractFull } = Seven;
+const isLinux = process.platform === 'linux';
 
 export default async function unzip(zipPath, outputPath) {
   const zipStream = extractFull(zipPath, outputPath, {
@@ -10,6 +12,8 @@ export default async function unzip(zipPath, outputPath) {
 
   return new Promise((resolve) => {
     zipStream.once('end', () => {
+      if (isLinux)
+        execSync(`convmv -f shift_jis -t utf8 -r --notest ${outputPath}`);
       resolve();
     });
   });
